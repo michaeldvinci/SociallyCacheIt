a046313cc17c26d4abe012500e8158614b8f939e
Michael Vinci <michaeldvinci@gmail.com>
2016-12-09 18:46:06 -0500

    Before Packaging

--- reroute.js
+++ reroute.js
@@ -5,13 +5,6 @@
 //	Michael D. Vinci
 //
 //
-//	http://webcache.googleusercontent.com/search?q=cache:
-//
-// 	https://social.technet.microsoft.com*
-// 
-//	&num=1&hl=en&gl=us&strip=1&vwsrc=0
-//
-//
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
